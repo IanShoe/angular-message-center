8d4906bbe108b4f34350761c1b35904a7b7668b8
Ian Shoemaker <iss90g@gmail.com>
2013-10-22 14:46:30 -0400

    better markup better css better animation queue implementation colors

--- message-center-templates.js
+++ message-center-templates.js
@@ -2,18 +2,18 @@ angular.module("template/message-center/message-center.html", []).run(["$templat
   $templateCache.put("template/message-center/message-center.html",
     '<span>' +
         '<span class="message-center-important">' +
-            '<message ng-repeat="messageItem in impMessageItems" class="message-animation"></message>' +
+            '<message ng-repeat="message in impMessages" class="message-animation"></message>' +
         '</span>' +
         '<span class="message-center-regular">' +
-            '<message ng-repeat="messageItem in messageItems" class="message-animation"></message>' +
+            '<message ng-repeat="message in messages" class="message-animation"></message>' +
         '</span>' +
     '<span>');
 }]);
 
 angular.module("template/message-center/message.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("template/message-center/message.html",
-    '<div class="message-box" ng-class="messageItem.class">' +
-        '<button type="button" class="close" aria-hidden="true" ng-click="removeItem(messageItem)">&times;</button>' +
-        '<span class="message-item">{{messageItem.message}}</span>' +
+    '<div class="message-box" ng-class="message.classes">' +
+        '<span class="message">{{message.message}}</span>' +
+        '<button type="button" class="close" aria-hidden="true" ng-click="removeItem(message)">&times;</button>' +
     '</div>');
 }]);
--- message-center.js
+++ message-center.js
@@ -1,43 +1,48 @@
 angular.module('MessageCenter', ['ngAnimate','template/message-center/message.html', 'template/message-center/message-center.html']).
 factory('MessageService', function ($rootScope) {
-    
+
     var MessageService = {};
     MessageService.config = {
-        disabled: false
+        disabled: false,
+        max: 3,
+        timeout: 3000
     };
 
     var history = [];
     var counter = 0;
 
     MessageService.configure = function(config){
+        //need to make sure that omitted values don't override current values
         this.config.disabled = angular.isDefined(config.disabled) ? config.disabled : this.config.disabled;
+        this.config.max = angular.isDefined(config.max) ? config.max : this.config.max;
+        this.config.timeout = angular.isDefined(config.timeout) ? config.timeout : this.config.timeout;
     };
 
-    MessageService.broadcast = function (message, opts) {
+    MessageService.broadcast = function (msg, opts) {
         if(!this.config.disabled){
             counter++;
-            var messageItem = {};
-            // Should probably do this better since I tailored this to my own needs
-            // I didn't want the div to automatically grow based on the message size,
-            // but rather have distinct cut off points
-            if(message.length > 26){
-                messageItem.class = 'large';
-            }
-            else if(message.length < 18){
-                messageItem.class = 'small';
-            }
+            var message = {
+                classes: [],
+                message: msg,
+                id: counter,
+                timeout: MessageService.config.timeout
+            };
             if(opts){
                 if(opts.important){
-                    messageItem.type = 'important';
+                    message.type = 'important';
+                }
+                if(opts.color){
+                    message.classes.push(opts.color);
+                }
+                if(angular.isDefined(opts.timeout) && angular.isNumber(opts.timeout)){
+                    message.timeout = opts.timeout;
                 }
             }
-            messageItem.message = message;
-            messageItem.id = counter;
-            history.push(messageItem);
-            $rootScope.$broadcast('MessageService.broadcast', messageItem);
+            history.push(message);
+            $rootScope.$broadcast('MessageService.broadcast', message);
         }
         else {
-            console.log('Message service disabled for message: '+ message);
+            console.log('Message service disabled for message: '+ msg);
         }
     };
 
@@ -58,18 +63,50 @@ directive('messageCenter', function ($timeout) {
         scope: {},
         templateUrl: 'template/message-center/message-center.html',
         controller: function ($scope, $attrs, MessageService) {
-            $scope.messageItems =  [];
-            $scope.impMessageItems =  [];
+
+            $scope.messages =  [];
+            $scope.impMessages =  [];
+            var queue = [];
+            var impQueue = [];
+
             $scope.$on('MessageService.broadcast', function (event, message) {
-                message.type ? $scope.impMessageItems.push(message) : $scope.messageItems.push(message);
-                $timeout(function(){
-                    $scope.removeItem(message);
-                }, 3000);
+                var q, list;
+                if(message.type){
+                    q = impQueue;
+                    list = $scope.impMessages;
+                }
+                else {
+                    q = queue;
+                    list = $scope.messages;
+                }
+                q.push(message);
+                if(list.length < MessageService.config.max && q.length == 1){
+                    // if it's the first item and the max hasn't been hit yet, then start processing
+                    processQueue(q, list);
+                }
+                else{
+                    console.log('saved '+ message.id + ' to queue');
+                }
             });
-            $scope.removeItem = function (message){
-                $scope.$emit('MessageService.remove', message);
-                message.type ? remove($scope.impMessageItems, message) : remove($scope.messageItems, message);
-            };
+
+            function processQueue(q, list){
+                if(q.length == 0){
+                    return;
+                }
+                var nextMsg = q.shift();
+                list.push(nextMsg);
+                console.log('adding message ' + nextMsg.id);
+                
+                $timeout(function(){
+                    remove(list, nextMsg);
+                    if(q.length > 0){
+                        console.log('about to add' + nextMsg.id);
+                        $timeout(function(){
+                            processQueue(q, list);
+                        }, 300);
+                    }
+                }, nextMsg.timeout);
+            }
 
             function remove(array, item){
                 var index = array.indexOf(item);
